51eacb915ab2a29535e97dd2abd5b96eb270df25
gugumao <850153593@qq.com>
2022-08-08 17:32:58 +0800

    变更

---
+++ src/main/java/com/learn/git.java
@@ -0,0 +1,11 @@
+package com.learn;
+
+/**
+ * @author 咕咕猫
+ * @version 1.0
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
