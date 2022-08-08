43ecd3e02c1feebbf4d1c2924b5f0aed46dbc58a
gugumao <850153593@qq.com>
2022-08-08 17:47:05 +0800

    third commit

--- src/main/java/com/learn/git.java
+++ src/main/java/com/learn/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git!");
+        System.out.println("hello,git3!");
     }
 }
